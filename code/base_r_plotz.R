750bdc55e7c84318a3127dbae186b5bdeeaad6f1
rlsc-40 <wildredpoppy@gmail.com>
2019-04-04 17:15:27 -0700

    corrected file placement code & data

--- code/base_r_plotz.R
+++ code/base_r_plotz.R
@@ -17,12 +17,21 @@ stime <- hms(biketown$StartTime)
 biketown$hour <- hour(stime)
 
 table(biketown$hour)
-#hms --> hour, minute, second; we extracted the hour
+# hms --> hour, minute, second; we extracted the hour
 freq_by_hr <- table(biketown$hour)
 barplot(freq_by_hr)
 barplot(40000,freq_by_hr) #this did not work
+
+# looking at seasonal patterns
 freq_by_month <- table(biketown$month)
-barplot(freq_by_month)
+barplot(freq_by_month) # originally gave to us in alphabetical order d/t being brought in as a factor
+# needed to do the code below to change order
+biketown$month <- 
+  mdy(biketown$StartDate) %>%
+  month(label = T, abbr = T)
+str(biketown$month) # just to check
+
+
 
 # investigating hourly bins
 hist(biketown$hour) #computer chooses breaks
@@ -36,3 +45,23 @@ am_peak <- subset(biketown, hour >= 7 & hour < 10) # this is how we write an if;
 hist(am_peak$hour, breaks = seq(7, 10, 1)) # not very informative
 barplot(table(am_peak$hour))
 pm_peak <- biketown[levels(biketown$hour) >= 16 & hour < 20, ] # this is not working
+pm_peak <- subset(biketown, hour >= 16 & hour < 20)
+hist(pm_peak$hour)
+
+# looking by station
+summary(biketown)
+str(biketown) # reminding myself of the number of stations
+freq_by_station <- table(biketown$StartHub)
+
+# top 25 stations
+s <- sort(freq_by_station, decreasing = T)
+top25 <- s[1:25]
+# could have written: top25 <- sort(freq_by_station, decreasing = T)[1:25]
+dotchart(top25)
+
+# to remove a var, just rm(varname == "values")
+# pipe function (%>%) is only in R, saves a lot of time over time
+
+
+
+
